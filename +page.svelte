80582886dd4dabfdbac894e2cc73f98c9641b208
Gavin Fogel <gavin@fogel.io>
2024-05-13 09:36:43 -0700

    push site

---
+++ +page.svelte
@@ -0,0 +1,1 @@
+<div class="text-white">The Sea Peoples</div>
---
+++ src/routes/references/+page.svelte
@@ -0,0 +1,18 @@
+<div class="w-full flex items-center justify-center">
+	<div class="max-w-5xl p-12 text-neutral-100">
+		<h1 class="text-5xl">References</h1>
+		<div class="mt-2 text-neutral-300 text-sm">(In Harvard style citation format)</div>
+		<ul class="flex flex-col space-y-8 text-2xl mt-24">
+			<li>
+				GORDON DOHERTY, AUTHOR. (n.d.). Death from the Waves - the Sea People. [online] Available
+				at: https://www.gordondoherty.co.uk/writeblog/death-from-the-waves-the-sea-people [Accessed
+				1 May 2024].
+			</li>
+			<li>
+				www.youtube.com. (n.d.). Who were the Sea Peoples? | The Bronze Age Collapse. [online]
+				Available at: https://www.youtube.com/watch?v=q9jqwLYeVtk&ab_channel=ReligionForBreakfast
+				[Accessed 1 May 2024].
+			</li>
+		</ul>
+	</div>
+</div>
